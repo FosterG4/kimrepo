7c84d186e2ef47ff50f895786978936008d4d5bb
Dikky Hardian <30888372+FosterG4@users.noreply.github.com>
2022-10-31 12:47:23 +0700

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -7,7 +7,7 @@ sleep 7
 cd ~/
 cp /opt/kimrepo/asset/MariaDB.repo /etc/yum.repos.d/MariaDB.repo
 
-#install mariadb 10.1
+#install mariadb
 yum install boost-devel.x86_64 socat MariaDB-server MariaDB-client MariaDB-compat galera socat jemalloc rsync galera -y
 sleep 2
 
