89e0e06caee8010d2012c34febcf2c3ddf2771cc
Dikky Hardian <30888372+FosterG4@users.noreply.github.com>
2023-11-24 11:02:58 +0700

    Update timedate.sh

--- timedate.sh
+++ timedate.sh
@@ -2,6 +2,7 @@
 ENV TZ=Asia/Jakarta
 
 # symlink localtimezone
+#ln -snf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime && echo Asia/Jakarta > /etc/timezone
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 #install tzdata
